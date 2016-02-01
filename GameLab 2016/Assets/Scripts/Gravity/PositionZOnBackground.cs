c2e762ae7ac5e0c261f69b9038c887ae9ccc2124
Alexis <alex3521@hotmail.fr>
2016-02-01 16:33:11 -0500

    Added Documentation, Tweaked Prefabs

--- GameLab 2016/Assets/Scripts/Gravity/PositionZOnBackground.cs
+++ GameLab 2016/Assets/Scripts/Gravity/PositionZOnBackground.cs
@@ -6,11 +6,9 @@ using System.Collections;
 /// </summary>
 public class PositionZOnBackground : MonoBehaviour {
 
-    [SerializeField] private float _zOffset = 0f;    
-    [SerializeField] private LayerMask _backgroundLayerMask;
+    [Tooltip("Offset on z Axis from the z value of the background")][SerializeField] private float _zOffset = 0f;    
+    [Tooltip("DO NOT TOUCH. Layers of the background collision.")][SerializeField] private LayerMask _backgroundLayerMask;
 
-    private bool drawGizmo = false;
-    private Vector3 GizmoLocation = Vector3.zero;
 	// Update is called once per frame
 	void Update () {
         RepositionObjectZ();
@@ -26,23 +24,15 @@ public class PositionZOnBackground : MonoBehaviour {
         float targetZ = 0;
 
         Debug.DrawRay(transform.position, new Vector3(0, 0, 1000));
-        if (Physics.Raycast(rayZ, out hit, 1000, _backgroundLayerMask)) {
-            GizmoLocation = hit.point;
-            //drawGizmo = true;
-            Debug.Log("blabla");
-            
+        if (Physics.Raycast(rayZ, out hit, 1000, _backgroundLayerMask)) {       
             targetZ = hit.point.z;        
         } else if(Physics.Raycast(rayInverseZ, out hit, 1000, _backgroundLayerMask)) {
             targetZ = hit.point.z;
         }
 
+        targetZ += _zOffset;
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, targetZ);
         transform.position = newPosition;
     }
 
-    void OnDrawGizmos() {
-        if(drawGizmo) {
-            Gizmos.DrawSphere(GizmoLocation, 1f);
-        }
-    }
 }
