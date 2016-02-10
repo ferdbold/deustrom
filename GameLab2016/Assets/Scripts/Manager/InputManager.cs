5b701adf152be10350efcae8a7eee54b646904bd
Antoine Brouillette <antoineqc@hotmail.com>
2016-02-10 17:12:05 -0500

    Change to Input Manager to add new Inputs

--- GameLab2016/Assets/Scripts/Manager/InputManager.cs
+++ GameLab2016/Assets/Scripts/Manager/InputManager.cs
@@ -10,13 +10,17 @@ public class InputManager : MonoBehaviour {
 	public delegate void EventButton();
 
 	//Possible Event
-	public EventAxis leftAnalog { get; private set; }
+	private EventAxis _leftAnalog;
+	private EventAxis _rightAnalog;
+
+	private EventButton _startButton;
+	private EventButton _fireHookButton;
 
 	#region Add Event
 	/// <summary>
 	/// The name of every axis
 	/// </summary>
-	public enum Axis { leftAnalog }
+	public enum Axis { leftAnalog, rightAnalog }
 	/// <summary>
 	/// Link a function to an event
 	/// </summary>
@@ -24,7 +28,8 @@ public class InputManager : MonoBehaviour {
 	/// <param name="eventFunction">The function to be call when the event is triggered</param>
 	public void AddEvent(Axis axis, EventAxis eventFunction) {
 		switch (axis) {
-			case Axis.leftAnalog: leftAnalog += eventFunction; break;
+			case Axis.leftAnalog: _leftAnalog += eventFunction; break;
+			case Axis.rightAnalog: _rightAnalog += eventFunction; break;
 
 			//Error
 			default:
@@ -37,7 +42,7 @@ public class InputManager : MonoBehaviour {
 	/// <summary>
 	/// The name of every buttons
 	/// </summary>
-	public enum Button { }
+	public enum Button { start, fireHook }
 	/// <summary>
 	/// Link a function to an event
 	/// </summary>
@@ -45,6 +50,8 @@ public class InputManager : MonoBehaviour {
 	/// <param name="eventFunction">The function to be call when the event is triggered</param>
 	public void AddEvent(Button button, EventButton eventFunction) {
 		switch (button) {
+			case Button.start: _startButton += eventFunction; break;
+			case Button.fireHook: _fireHookButton += eventFunction; break;
 
 			//Error
 			default:
@@ -57,8 +64,14 @@ public class InputManager : MonoBehaviour {
 
 	void Update() {
 		try {
-			//Called function linked to 
-			leftAnalog.Invoke(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+			/* Called function linked to */
+			//Axis
+			_leftAnalog.Invoke(Input.GetAxis("L_Horizontal"), Input.GetAxis("L_Vertical"));
+			_rightAnalog.Invoke(Input.GetAxis("R_Horizontal"), Input.GetAxis("R_Horizontal"));
+
+			//Button
+			if (Input.GetButtonDown("Fire Hook")) _fireHookButton.Invoke();
+			if (Input.GetButtonDown("Start")) _startButton.Invoke();
 
 		}
 		catch (NullReferenceException e) {
