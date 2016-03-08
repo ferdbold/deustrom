45fe683023979457355c1c8bd81cf1bfc2658244
sprawls <alex3521@hotmail.fr>
2016-03-08 11:00:06 -0500

    Cleaned folders and started Island Feeders

---
+++ GameLab2016/Assets/Scripts/AI/IslandFeeder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandFeeder : MonoBehaviour {
+
+    [Header("Continent Properties")]
+    [SerializeField] [Tooltip("Prefab of normal island")]
+    private GameObject _islandPrefab;
+    [SerializeField]
+    [Tooltip("Number of island in each column")]
+    private int height = 5;
+
+    [Header("Spawn Properties")]
+    [SerializeField] [Tooltip("Basic Island Spawn Rate")]
+    private float _spawnRate;
+
+
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
