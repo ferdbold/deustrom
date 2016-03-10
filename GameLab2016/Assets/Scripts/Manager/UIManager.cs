6ee59fec998dcd9a60846f14ebb50b35c7f67e49
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-10 15:41:35 -0500

    Score widget stubs

--- GameLab2016/Assets/Scripts/Manager/UIManager.cs
+++ GameLab2016/Assets/Scripts/Manager/UIManager.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using Simoncouche.UI;
 
+/// <summary>The UIManager oversees the entire UI layer of the game.</summary>
 public class UIManager : MonoBehaviour {
+    
+    private List<ScoreWidget> _scoreWidgets;
 
-    void Start () {
-    
+    private void Awake() {
+        _scoreWidgets = new List<ScoreWidget>();
+        _scoreWidgets.Add(GameObject.Find("UI/Scores/Sobek").GetComponent<ScoreWidget>());
+        _scoreWidgets.Add(GameObject.Find("UI/Scores/Cthulhu").GetComponent<ScoreWidget>());
     }
-    
-    void Update () {
-    
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.K))
+            _scoreWidgets[0].AddPoints(10);
+
+        if (Input.GetKeyDown(KeyCode.L))
+            _scoreWidgets[1].AddPoints(10);
     }
 }
---
+++ GameLab2016/Assets/Scripts/UI/ScoreWidget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Simoncouche.UI {
+    /// <summary>A ScoreWidget tracks the points accumulated by a single player under the form of a parchment slider.</summary>
+    [RequireComponent(typeof(Slider))]
+    public class ScoreWidget : MonoBehaviour {
+
+        [SerializeField]
+        private float GAIN_ANIM_DURATION = 0.5f;
+
+        // COMPONENTS
+
+        private Slider _slider;
+
+        // METHODS
+
+        private void Awake () {
+            _slider = GetComponent<Slider>();
+        }
+
+        public void AddPoints(int amount) {
+            _slider.DOValue(_slider.value + amount, this.GAIN_ANIM_DURATION).SetEase(Ease.OutCubic); 
+        }
+    }
+}
