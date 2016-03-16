76504ea3bfaafc1268cb17964abe4c0b13110932
MarcAndreFL <marcfl20@gmail.com>
2016-03-16 17:20:59 -0400

    Debut du travail sur le BibleReader + quelques méthodes de + dans BibleEntries

---
+++ GameLab2016/Assets/Scripts/Bible/BibleQuoteReader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BibleQuoteReader : MonoBehaviour {
+
+    private int _currentGodToCheck;
+
+    void Awake() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+}
