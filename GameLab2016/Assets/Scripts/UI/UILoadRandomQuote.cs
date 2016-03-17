6001527a020e47c30f1a74357e3f07fbe809e682
MarcAndreFL <marcfl20@gmail.com>
2016-03-17 16:48:46 -0400

    Cutscene avec quote

---
+++ GameLab2016/Assets/Scripts/UI/UILoadRandomQuote.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Simoncouche.Bible;
+using UnityEngine.UI;
+
+public class UILoadRandomQuote : MonoBehaviour {
+
+    private Text textElementVerseQuote;
+    private Text textElementAuthorAndNo;
+
+    void Awake() {
+        Text[] array = GetComponentsInChildren<Text>();
+        textElementVerseQuote = array[0];
+        textElementAuthorAndNo = array[1];
+    }
+
+	// Use this for initialization
+	void Start () {
+        BibleQuote bibleQuote = BibleEntries.GetRandomQuote();
+        textElementVerseQuote.text = bibleQuote.quoteString;
+        textElementAuthorAndNo.text = "- " + bibleQuote.godName + " " + bibleQuote.quoteFirstNo + ":" + bibleQuote.quoteSecondNo;
+    }
+}
