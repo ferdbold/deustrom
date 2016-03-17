f7c5cca72b965d6062a7cceb16edceeef2e4dfbb
Marc-André Fortin-Leclerc <marc1999_8@hotmail.com>
2016-03-17 00:18:11 -0400

    Début du BibleReader

--- GameLab2016/Assets/Scripts/Bible/BibleQuoteReader.cs
+++ GameLab2016/Assets/Scripts/Bible/BibleQuoteReader.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+namespace Simoncouche.Bible {
+    public class BibleQuoteReader : MonoBehaviour {
 
-public class BibleQuoteReader : MonoBehaviour {
+        private bool _currentGodToCheckIsSobek=false;
+        private Text _textBible;
 
-    private int _currentGodToCheck;
+        void Awake() {
+            _textBible = GetComponentInChildren<Text>();
+        }
 
-    void Awake() {
+        // Use this for initialization
+        void Start() {
+            _textBible.text = GetQuotesOfGod(_currentGodToCheckIsSobek);
+        }
 
+        private string GetQuotesOfGod(bool isSobek) {
+            string quotes="";
+            BibleEntries bibleEntries = BibleEntries.LoadBibleEntries();
+            if (bibleEntries == null) {
+                return quotes = "Aucun texte sacré n'a été écrit pour " + (isSobek ? "Sobek" : "Cthulu");
+            } else {
+                if (isSobek) {
+                    if (bibleEntries.quoteListSobek.Count < 1) return quotes = "Aucun texte sacré n'a été écrit pour " + (isSobek ? "Sobek" : "Cthulu");
+                    foreach (BibleQuote bQuote in bibleEntries.quoteListSobek) {
+                        quotes += bQuote.quoteString + "\n\n\t\t" + " - " + bQuote.godName + " " + bQuote.quoteFirstNo + ":" + bQuote.quoteSecondNo + "\n\n";
+                    }
+                    return quotes;
+                } else {
+                    if (bibleEntries.quoteListCthulu.Count < 1) return quotes = "Aucun texte sacré n'a été écrit pour " + (isSobek ? "Sobek" : "Cthulu");
+                    foreach (BibleQuote bQuote in bibleEntries.quoteListCthulu) {
+                        quotes += bQuote.quoteString + "\n\n\t\t" + " - " + bQuote.godName + " " + bQuote.quoteFirstNo + ":" + bQuote.quoteSecondNo + "\n\n";
+                    }
+                    return quotes;
+                }
+            }
+
+        }
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-
 }
