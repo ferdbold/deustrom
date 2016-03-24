c15c0341c0a620327bef52276569cf2a809edd50
Antoine Brouillette <abrouillette@nad.qc.ca>
2016-03-23 20:30:11 -0400

    SIMONCOUCHE

---
+++ Gamelab2016/Assets/Scripts/UI/SimoncoucheIntro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SimoncoucheIntro : MonoBehaviour {
+
+    MovieTexture movie;
+
+    public RawImage image;
+
+	void Start () {
+        movie = (MovieTexture)image.mainTexture;
+        movie.Play();
+        StartCoroutine(Wait());
+	}
+
+    IEnumerator Wait() {
+        while (movie.isPlaying) {
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene("Menu");
+    }
+}
